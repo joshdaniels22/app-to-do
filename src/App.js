0b069f336057c223dffb5613cd33bf7ce4c828d9
Joshua Daniels <joshuadaniels537@gmail.com>
2023-06-25 22:10:52 +0200

    feature: headings and basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
 import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
 function App() {
+
+  // let form = document.getElementById('form');
+  // form.onsubmit = handleFormSubmit;
+
+
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleMenuOne = () => {
+    // do something
+    setOpen(false);
+  };
+
+  const handleMenuTwo = () => {
+    // do something
+    setOpen(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TODO LIST</h1>
+
+
+
+      <textarea id="textarea">hsai</textarea>
+      <button id="button1">
+      Add Task
+    </button>
+  
+
+    <Dropdown
+      open={open}
+      trigger={<button onClick={handleOpen}>Dropdown</button>}
+      menu={[
+        <button onClick={handleMenuOne}>Menu 1</button>,
+        <button onClick={handleMenuTwo}>Menu 2</button>,
+      ]}
+    />
+          <ul>
+  <li>Buy Milk</li>
+  <li>Clean Room</li>
+  <li>Play Squash</li>
+</ul>
       </header>
+
     </div>
   );
+
+
 }
+
+const Dropdown = ({ open, trigger, menu }) => {
+  return (
+    <div className="dropdown">
+      {trigger}
+      {open ? (
+        <ul className="menu">
+          {menu.map((menuItem, index) => (
+            <li key={index} className="menu-item">{menuItem}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+
+
+
+// async function handleFormSubmit(e) {
+//   e.preventDefault();
+//   disable(textarea);
+//   disable(button);
+//   show(loadingMessage);
+//   hide(errorMessage);
+//   try {
+//     await submitForm(textarea.value);
+//     show(successMessage);
+//     hide(form);
+//   } catch (err) {
+//     show(errorMessage);
+//     errorMessage.textContent = err.message;
+//   } finally {
+//     hide(loadingMessage);
+//     enable(textarea);
+//     enable(button);
+//   }
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,29 @@ root.render(
   </React.StrictMode>
 );
 
+// let textarea = document.getElementById('textarea');
+// textarea.oninput = handleTextareaChange;
+
+// var newTask
+
+// function handleTextareaChange() {
+//   newTask = 'new one'
+
+//   alert('Adding Task!');
+// }
+
+let button = document.getElementById('button1');
+button.onclick = handleButtonClick;
+
+function handleButtonClick() {
+  // newTask = 'new one'
+
+  alert('Adding Task!');
+}
+
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
