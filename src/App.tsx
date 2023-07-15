3dab2f5700ec74943ed182146a0f9bc856e58b65
Joshua Daniels <joshuadaniels537@gmail.com>
2023-07-15 02:06:15 +0200

    fix: clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,31 @@
-import React, { ChangeEvent } from "react";
+import { ChangeEvent } from "react";
 import { useState } from "react";
 
 function App() {
-  // const [open, setOpen] = React.useState(false);
-  // var tasks = ["Laundry", "Dishes", "Trash", "Shopping"];
-
-  const [tasks, setValue] = useState(["Laundry", "Dishes", "Trash", "Shopping"]);
+  const [taskList, setTaskList] = useState([""]);
   const [newTask, setNewTask] = useState("");
-  // setValue(["Laundry", "Dishes", "Trash", "Shopping"]);
+
   function handle() {
-    // tasks.push(newTask)
-    setValue(current => [...current, newTask])
-    // console.log(tasks)
+    setTaskList((current) => [...current, newTask]);
   }
 
   function handleText(e: ChangeEvent<HTMLInputElement>) {
-    // 👇 Store the input value to local state
     setNewTask(e.target.value);
-  };
-
-
-  
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>TODO LIST</h1>
 
+        <input onChange={handleText} />
 
-        <input
-          onChange={handleText}
-        />
         <button onClick={handle}>Add Task</button>
 
-        {/* <Dropdown
-      open={open}
-      trigger={<button onClick={handleOpen}>Dropdown</button>}
-      menu={[
-        <button onClick={handleMenuOne}>Menu 1</button>,
-        <button onClick={handleMenuTwo}>Menu 2</button>,
-      ]}
-    /> */}
-
         <ul>
-          {tasks.map((item, index) => (
-            <li
-            // className={
-            //   index === selectedIndex
-            //     ? "list-group-item active"
-            //     : "list-group-item"
-            // }
-            // key={item}
-            // onClick={() => {
-            //   setSelectedIndex(index);
-            // }}
-            >
-              {item}
-            </li>
+          {taskList.map((item) => (
+            <li>{item}</li>
           ))}
-          {/* // i>
-        // ))}
-        
-        //   <li>{tasks.map()}</li>
-        //   <li>Clean Room</li>
-        //   <li>Play Squash</li> */}
         </ul>
       </header>
     </div>
