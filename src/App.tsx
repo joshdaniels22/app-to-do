d761ff3c46eb7f9d613bd61258818899ddbe0b5b
Joshua Daniels <joshuadaniels537@gmail.com>
2023-07-15 20:55:14 +0200

    feature: Mark task done and delete task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { ChangeEvent } from "react";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 function App() {
-  const [taskList, setTaskList] = useState([""]);
+  const [taskList, setTaskList] = useState(["Make to do list..."]);
   const [newTask, setNewTask] = useState("");
 
-  function handle() {
+  function handleAddTask() {
     setTaskList((current) => [...current, newTask]);
+  }
+
+  function handleDeleteTask(taskIndex: number) {
+    setTaskList((current) =>
+      current.filter((_, index) => {
+        return index !== taskIndex;
+      })
+    );
   }
 
   function handleText(e: ChangeEvent<HTMLInputElement>) {
@@ -18,13 +25,42 @@ function App() {
       <header className="App-header">
         <h1>TODO LIST</h1>
 
-        <input onChange={handleText} />
+        <input
+          onChange={handleText}
+          type="text"
+          className="form-control"
+          placeholder="New Task"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
 
-        <button onClick={handle}>Add Task</button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleAddTask}
+        >
+          Add Task
+        </button>
 
-        <ul>
-          {taskList.map((item) => (
-            <li>{item}</li>
+        <ul className="list-group">
+          {taskList.map((item, index) => (
+            <li className="list-group-item">
+              <input
+                className="form-check-input me-1"
+                type="checkbox"
+                value=""
+              />
+              <label className="form-check-label">{item}</label>
+
+              <button
+                onClick={() => {
+                  handleDeleteTask(index);
+                }}
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+              />
+            </li>
           ))}
         </ul>
       </header>
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
